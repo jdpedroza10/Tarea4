76677fa38e912bb8ebc7e94664852184947a962c
jdpedroza10 <42391174+jdpedroza10@users.noreply.github.com>
2018-11-19 16:07:47 -0500

    makefile definitivo

--- hw4.mk
+++ hw4.mk
@@ -3,7 +3,8 @@ all: hw4.pdf
 
 hw4.pdf: average_temperature_caso_1.png  caso_1_t0s.png  trayectoria_70.png
 	# compilar con pdflatex
-	pdflatex_Resultados hw4.tex
+	pdflatex Resultados_hw4.tex
+	#oprimir enter a las preguntas
 
 average_temperature_caso_1.png  caso_1_t0s.png  trayectoria_70.png: solution_1.txt solution_2.txt solution_3.txt a10.txt a20.txt a30.txt a40.txt a45.txt a50.txt a60.txt a70.txt
 	python3 Plots_hw4.py
